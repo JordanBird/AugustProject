6d08b7c6c604b643b3c2571fb0cc52fd02eefcdf
Jordan Bird <jordanbird-29@hotmail.co.uk>
2013-08-12 15:56:38 +0100

    Answer Debug Dummy
    Answer now has a variable 'type' which is an int 0 = Text and 1 = Image.
    It is currently locked to equal 0.

--- KittenPawPrintGame/Main Project/Assets/Answer.cs
+++ KittenPawPrintGame/Main Project/Assets/Answer.cs
@@ -5,6 +5,9 @@ public class Answer
 	public bool correct;
 	public string text;
 	
+	//Type, 0 = Text, 1 = Image.
+	public int type = 0;
+	
 	public Answer(bool iCorrect, string iText)
 	{
 		text = iText;
