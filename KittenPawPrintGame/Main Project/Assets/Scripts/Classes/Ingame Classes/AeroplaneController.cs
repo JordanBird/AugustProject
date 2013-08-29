b680f95d84fb569c2276c3f1327627c7daa43906
Alexander Saye <alexandersaye@live.co.uk>
2013-08-29 18:34:30 +0100

    Started Plane Game
    Took long enough just to figure out why unity was breaking everything...
    =_=

---
+++ KittenPawPrintGame/Main Project/Assets/Scripts/Classes/Ingame Classes/AeroplaneController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AeroplaneController : MonoBehaviour
+{
+	public Transform SkinPrefab;
+	private Transform skin;
+	private float tilt;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		skin = (Transform)Instantiate (SkinPrefab, transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+	
+	void FixedUpdate()
+	{
+		// DEBUG BECAUSE YOU CANT TEST ON PC WITH TILT!		
+		if (Input.GetKey (KeyCode.D))
+		{
+			tilt = Mathf.Lerp (tilt, -1, 0.1f);
+		}
+		
+		if (Input.GetKey(KeyCode.A))
+		{
+			tilt = Mathf.Lerp (tilt, 1, 0.1f);
+		}
+		
+		//tilt = iPhoneInput.acceleration.y;
+		//tilt *= Time.fixedDeltaTime;
+		
+		skin.position = transform.position;
+		skin.rotation = Quaternion.Lerp (skin.rotation, Quaternion.Euler(new Vector3(0, 0,  90 + Mathf.Clamp (60 * tilt, -30, 30))), 0.1f);	// yeah....you cant rotate children so i had to make the part which rotates seperately. Go Unity!....¬___¬
+		
+		rigidbody.velocity = Vector3.zero;
+		
+		rigidbody.AddForce(Vector3.up * tilt * 400);
+	}
+}
