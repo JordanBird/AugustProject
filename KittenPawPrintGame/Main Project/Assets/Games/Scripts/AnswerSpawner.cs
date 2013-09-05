5a3905b3b19c4c9351da52fc53643de0ae1a3e71
Alexander Saye <alexandersaye@live.co.uk>
2013-09-05 14:38:00 +0100

    AnswerSpawner, Question and AnswerCollectable
    AnswerSpawner now written nicer and better functioning.
    Question has a new method to return the number of correct answers
    (perhaps add another method to return an array containing only the
    correct answers?)
    AnswerCollectable prefab added.

--- KittenPawPrintGame/Main Project/Assets/Games/Scripts/AnswerSpawner.cs
+++ KittenPawPrintGame/Main Project/Assets/Games/Scripts/AnswerSpawner.cs
@@ -6,13 +6,15 @@ public class AnswerSpawner : MonoBehaviour
 {
 	public Transform AnswerPrefab;
 	public float SpawnDelay;
-	public float NextQuestionDelay;
+	public float TransitionDelay;
 	
 	private float spawnTimer = 0;
-	private float nextQuestionTimer = 0;	
-	private int lastAnswerSpawned = 0;
+	private float transitionTimer = 0;
+	private bool transitioning = true;
 	private Question currentQuestion;
 	private List<Answer> answerBatch = new List<Answer>();
+	private List<Answer> correctAnswers = new List<Answer>();
+	private List<Transform> answerObjects = new List<Transform>();
 	
 	void Start ()
 	{
@@ -21,45 +23,77 @@ public class AnswerSpawner : MonoBehaviour
 	
 	void Update ()
 	{
-		spawnTimer += Time.deltaTime;
-		if (spawnTimer >= SpawnDelay)
+		if (transitioning)
 		{
-			spawnTimer %= SpawnDelay;
+			// Display question text of next question until transition has finished? Darken screen during this to divert attention from the game towards the text?
 			
-			SpawnNextAnswer();
+			transitionTimer += Time.deltaTime;
+			if (transitionTimer >= TransitionDelay)
+			{
+				transitionTimer = 0;
+				
+				transitioning = false;
+			}
+		}
+		else
+		{
+			spawnTimer += Time.deltaTime;
+			if (spawnTimer >= SpawnDelay)
+			{
+				spawnTimer %= SpawnDelay;
+				
+				SpawnNextAnswer();
+			}
 		}
 		
-		/*
-		// -1 is being used as a flag to start and stop the timer.
-		if (nextQuestionTimer != -1)
-		{
-			nextQuestionTimer += Time.deltaTime;
-			if (nextQuestionTimer >= NextQuestionDelay)
-			{
-				nextQuestionTimer = -1;
-				
-				//currentQuestion = however we'll get the next question.
-			}
-		}*/
 	}
 	
 	private void SpawnNextAnswer()
 	{		
 		if (answerBatch.Count == 0)
 		{
-			answerBatch.AddRange(currentQuestion.answers);	// Refresh answers once all have been spawned.
+			// If the number of correct answers is equal to the correct answers collected, the player can proceed to the next question.
+			if (currentQuestion.GetNumberOfCorrectAnswers() == correctAnswers.Count)
+			{
+				LoadNextQuestion();
+				transitioning = true;
+				spawnTimer = 0;
+				return;
+			}
+			
+			// Refresh answers once all have previously been spawned.
+			answerBatch.AddRange(currentQuestion.answers);
+			
+			// Don't cycle correct answers that are already taken
+			for (int i = answerBatch.Count - 1; i >=0; i--)
+			{
+				if (answerBatch[i].correct == true)
+				{
+					for (int j = 0; j < correctAnswers.Count; j++)
+					{
+						if (answerBatch[i] == correctAnswers[j])
+						{
+							answerBatch.RemoveAt (i);
+							break;	
+						}
+					}
+				}
+			}
 		}
 		
 		int answer = Random.Range(0, answerBatch.Count);
 		
-		//AnswerPreab.Text = answerBatch[answer].Text;
-		Instantiate(AnswerPrefab);
+		// Add a new AnswerCollectable prefab with the text set to the answer's text.
+		AnswerPrefab.GetComponent<TextMesh>().text = answerBatch[answer].text;
+		answerObjects.Add ((Transform)Instantiate(AnswerPrefab));
 		
-		answerBatch.RemoveAt (answer);	// Remove the answer so it isn't used again until all others are used.
+		// Remove the answer so it isn't used again until all others are used.
+		answerBatch.RemoveAt (answer);
 	}
 	
-	/*public void EndCurrentQuestion()
+	private void LoadNextQuestion()
 	{
-		nextQuestionTimer = 0;
-	}*/
+		//currentQuestion = ;
+		
+	}
 }
